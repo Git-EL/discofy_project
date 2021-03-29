1a21129eeb0c969865d2799898b719d6111ed516
Suzy1412 <65951047+Suzy1412@users.noreply.github.com>
2021-03-29 14:31:36 +0200

    ne

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="blue-btn">
+       <a class="first-link" href="">
+         GET STARTED
+          </a>
+       <a href="">
+         CREATE YOUR PLAYLIST
+      </a>
+  </div>
+   
   );
 }
 
+
+
 export default App;
+
+
+
