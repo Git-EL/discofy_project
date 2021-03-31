332f7aaefe58d501495d5bfe5128876896c3d59e
Git-EL <lee_esther@live.com>
2021-03-31 12:22:27 +0200

    Spotify Login added

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,21 @@
 import './Intro.scss'
+import React from 'react'
 
-function Intro () {
+const Intro = () => {
+  const spotify_clientId = process.env.REACT_APP_CLIENT_ID
+  const spotify_authUrl = process.env.REACT_APP_AUTHORIZE_URL
+  // redirect muss noch geändert werden zur richtigen Adresse später
+  const spotify_redirectUrl = process.env.REACT_APP_REDIRECT_URL
+
+  const handleLogin = () => {
+    window.location = `${spotify_authUrl}?client_id=${spotify_clientId}&redirect_uri=${spotify_redirectUrl}&response_type=token&show_dialog=true`
+  }
+
   return <div className='testdiv'>
            <h2>Test-Text</h2>
+           <button variant='info' type='submit' onClick={handleLogin}>
+             Login with Spotify
+           </button>
          </div>
 }
 
