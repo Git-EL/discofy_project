e38990de93f6fe496c9a16640c17e553e25746e9
Git-EL <lee_esther@live.com>
2021-04-08 16:16:14 +0200

    background changes + scrollbar

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,14 @@ import Redirect from './Redirect/Redirect'
 import Selection from './Selection/Selection'
 import NotFound from './NotFound/NotFound'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import bgstyle1 from '../assets/bg-style1.svg'
-import bgstyle2 from '../assets/bg-style2.svg'
+
 
 function App () {
   return (
     <Router>
       <div className='App'>
         <Navbar />
-        <img src={bgstyle1} alt='bg-style' className='bgstyle1' />
-        <img src={bgstyle2} alt='bg-style' className='bgstyle2' />
-        <Switch>
+    <Switch>
           {/* Umleitung von '/' zu '/test'
                             <Redirect exact path='/' to='/test' />*/}
           <Route exact path='/' component={Home} />
