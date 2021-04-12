fa44e1eb7a81660d47c58e2836590fbd41146c07
Git-EL <lee_esther@live.com>
2021-04-13 01:43:09 +0200

    artisttracklist added + sound on mouseover

--- src/components/Selection/ArtistsTrackListe.js
+++ src/components/Selection/ArtistsTrackListe.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import './ArtistsTrackListe.scss'
+import React from "react";
+// import ReactAudioPlayer from 'react-audio-player';
+
 
 const ArtistsTrackListe = props => {
 
-  return (
-    <div className='col-sm-10'>
-      <div key={0}>
-        {props.artiststracklist.map((item, idx) => 
-        <div key={idx + 1} value={item.id}>
-          <div>{item}</div>
-            </div>)}
-      </div>
-    </div>
-  )
-}
+return (
+   <div className='col-sm-10'>
+     <div key={0} className="track-liste">
+       {props.artiststracklist.map((item, idx) =>  
+       { const audio = new Audio(item.preview_url);
+        function playtrack() { audio.play();}
+        function stoptrack() { audio.pause();}
+ return (
+       <div key={idx + 1} value={item.id} onMouseOver={playtrack} onMouseLeave={stoptrack}>
+         <img src={item.album.images[0].url} alt="test" className="trackalbum-image" id={item.id} width="100" height="100"/>
+       </div>)})}
+     </div>
+   </div>
+ )
+ }
 
 export default ArtistsTrackListe
